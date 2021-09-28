0a892263ead9ecb36975433014ded51b4b2ccf52
geramireza <r.gerami25@yahoo.com>
2021-09-28 21:49:41 +0330

    Change send message password to normal method

--- kara/app/Http/Controllers/Mobile/UserController.php
+++ kara/app/Http/Controllers/Mobile/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
         {
             Session::put(Constants::KEY_PASSWORD,$password);
             Session::put(Constants::KEY_EXPIRED_AT_PASSWORD,time() + 60 );
-            $result["result"] = User::ultraFastSend($phone,$password);
+            $result["result"] = User::sendMessage($phone,$password);
         }
         return response()->json($result);
     }
